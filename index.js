d84648e37b0c1b3a4e7ce7e60a9db057e0d49ab8
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2022-11-30 22:47:28 -0500

    feat: `npm start`

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'))
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+    console.log('http://localhost:3000');
+});
